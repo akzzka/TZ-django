0386bd1ea738a87110ab92e82464b5dc3978ada5
Fourbyseven <emkaumetov@gmail.com>
2024-10-24 03:23:48 +0600

    emka

--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    album = models.CharField(max_length=100, blank=True)
+    release_date = models.DateField()
+    genre = models.CharField(max_length=50, blank=True)
+    audio_file = models.FileField(upload_to='song/')
+
+    def __str__(self) -> str:
+        return f'{self.title}, {self.artist}'
+    
--- songs/views.py
+++ songs/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SongForm
 
-# Create your views here.
+def add_song(request):
+    if request.method == 'POST':
+        form = SongForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('some_view_name')
+    else:
+        form = SongForm()
+
+    return render(request, '')
+
