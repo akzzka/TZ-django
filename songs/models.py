70f24d793c374e908e1f772e5e09c41d7f0fe227
medina <medinaalievait.com>
2024-10-26 01:57:04 +0600

    medi

--- songs/models.py
+++ songs/models.py
@@ -5,6 +5,7 @@ from genre.models import Genre
 class Song(models.Model):
     title = models.CharField(max_length=100)
     artist = models.ForeignKey(Artist, related_name='songs', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product-image/')
     genre = models.ForeignKey(Genre, related_name='songs', on_delete=models.CASCADE)
     release_date = models.DateField()
 
